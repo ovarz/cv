2d9ead86bdb29b914e33f31ca4588e6c1783a79e
ovarz <ovarz_03@yahoo.com>
2021-06-06 01:32:31 +0700

    Update portfolio.php

--- module/portfolio.php
+++ module/portfolio.php
@@ -12,7 +12,7 @@
 	    <span>
 		  <h2>Social Media Generator</h2>
 		  <h4>Situs membuat gambar secara otomatis untuk kebutuhan tim sosial media VIVA Networks</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/social-media-generator/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="https://www.ov4rz.com/social-media-generator/" class="opl-button" target="_blank">www.ov4rz.com/social-media-generator/</a>
 		</span>
 	  </li>
     </ul>
@@ -25,7 +25,7 @@
 	    <span>
 		  <h2>New tvOne 2021</h2>
 		  <h4>Situs berita terkini tvonenews secara online paling lengkap, terbaru, dan yang sedang trending hari ini</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/tvone/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="https://www.ov4rz.com/tvone/" class="opl-button" target="_blank">www.ov4rz.com/tvone/</a>
 		</span>
 	  </li>
     </ul>
@@ -38,7 +38,7 @@
 	    <span>
 		  <h2>New VLIX 2021</h2>
 		  <h4>Situs yang menyajikan Kumpulan Video Hot Terbaru Seputar Berita, Olahraga, Otomotif, Gaya Hidup, Digital, Traveling. Serta Streaming tvOne, ANTV, dan Partner.</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/vlix/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="https://www.ov4rz.com/vlix/" class="opl-button" target="_blank">www.ov4rz.com/vlix/</a>
 		</span>
 	  </li>
     </ul>
@@ -51,7 +51,7 @@
 	    <span>
 		  <h2>New VIVA.co.id 2021</h2>
 		  <h4>Situs berita harian terkini yang menyajikan berita hari ini dan kabar terbaru seputar politik hingga berita lifestyle dan olahraga.</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/vivacoid/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="https://www.ov4rz.com/vivacoid/" class="opl-button" target="_blank">www.ov4rz.com/vivacoid/</a>
 		</span>
 	  </li>
     </ul>
@@ -64,20 +64,7 @@
 	    <span>
 		  <h2>New SIAPA 2021</h2>
 		  <h4>Situs kumpulan Tokoh Inspiratif dari Beberapa Profesi di Indonesia</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/siapa/" class="opl-button" target="_blank">Preview</a>
-		</span>
-	  </li>
-    </ul>
-	
-    <ul>
-	  <li class="opl-image content_center">
-	    <amp-img src="<?php echo $base?>/img/portofolio/porto-vivasso.jpg" width="160" height="90" layout="responsive" alt="VIVA SSO"></amp-img>
-	  </li>
-	  <li class="opl-info content_center">
-	    <span>
-		  <h2>VIVA Network User Dashboard</h2>
-		  <h4>Situs dashboard user VIVA mulai dari login, register, forgot password, profile, dll</h4>
-		  <a rel="noreferrer" href="https://www.ov4rz.com/vivasso/profile/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="https://www.ov4rz.com/siapa/" class="opl-button" target="_blank">www.ov4rz.com/siapa/</a>
 		</span>
 	  </li>
     </ul>
@@ -90,20 +77,7 @@
 	    <span>
 		  <h2>Pantau Corona COVID-19 Microsite</h2>
 		  <h4>Microsite Berita corona covid-19 di indonesia terbaru, fakta virus corona, pencegahan hingga hotline covid-19</h4>
-		  <a rel="noreferrer" href="https://www.viva.co.id/corona/" class="opl-button" target="_blank">Visit Site</a>
-		</span>
-	  </li>
-    </ul>
-	
-    <ul>
-	  <li class="opl-image content_center">
-	    <amp-img src="<?php echo $base?>/img/portofolio/porto-antvmicro.jpg" width="160" height="90" layout="responsive" alt="ANTV Microsite"></amp-img>
-	  </li>
-	  <li class="opl-info content_center">
-	    <span>
-		  <h2>ANTV Microsite</h2>
-		  <h4>Microsite Berita terbaru ANTV seperti jadwal program ANTV hari ini, live streaming ANTV, sinopsis, serta video dan foto terbaru.</h4>
-		  <a rel="noreferrer" href="https://www.viva.co.id/antv/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.viva.co.id/corona/" class="opl-button" target="_blank">www.viva.co.id/corona/</a>
 		</span>
 	  </li>
     </ul>
@@ -116,7 +90,7 @@
 	    <span>
 		  <h2>PT. Abirama Bandha Sejahtera</h2>
 		  <h4>Situs penyedia berbagai macam barang dan jasa untuk contractor, Alat Kesehatan, dan supply laboratorium.</h4>
-		  <a rel="noreferrer" href="https://abirama.net/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://abirama.net/" class="opl-button" target="_blank">abirama.net</a>
 		</span>
 	  </li>
     </ul>
@@ -129,7 +103,7 @@
 	    <span>
 		  <h2>One Pride Indonesia MMA</h2>
 		  <h4>Situs official One Pride, menyajikan kabar seputar berita, jadwal pertandingan, live streaming, angel, rank & biodata petarung</h4>
-		  <a rel="noreferrer" href="https://www.onepride.net/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.onepride.net/" class="opl-button" target="_blank">www.onepride.net</a>
 		</span>
 	  </li>
     </ul>
@@ -142,7 +116,7 @@
 	    <span>
 		  <h2>tvOne</h2>
 		  <h4>Situs Streaming Berita Terkini TvOne Online Terbaru dan Trending</h4>
-		  <a rel="noreferrer" href="https://www.tvonenews.com/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.tvonenews.com/" class="opl-button" target="_blank">www.tvonenews.com</a>
 		</span>
 	  </li>
     </ul>
@@ -155,20 +129,7 @@
 	    <span>
 		  <h2>IntipSeleb</h2>
 		  <h4>Situs yang menyajikan Gosip Artis Indonesia, Korea, Barat, Asia, Bollywood, termasuk musik, film dan drama terbaru hari ini.</h4>
-		  <a rel="noreferrer" href="https://www.intipseleb.com/" class="opl-button" target="_blank">Visit Site</a>
-		</span>
-	  </li>
-    </ul>
-	
-    <ul>
-	  <li class="opl-image content_center">
-	    <amp-img src="<?php echo $base?>/img/portofolio/porto-oneprix.jpg" width="160" height="90" layout="responsive" alt="OnePrix"></amp-img>
-	  </li>
-	  <li class="opl-info content_center">
-	    <span>
-		  <h2>OnePrix</h2>
-		  <h4>Situs Kejuaraan Nasional Balap Motor Oneprix – Indonesia Motorprix Championship Musim 2019 Bekerjasama dengan tvOne.</h4>
-		  <a rel="noreferrer" href="https://www.oneprix.id/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.intipseleb.com/" class="opl-button" target="_blank">www.intipseleb.com</a>
 		</span>
 	  </li>
     </ul>
@@ -181,7 +142,7 @@
 	    <span>
 		  <h2>100KPJ</h2>
 		  <h4>Situs Berita Motor, Balap Motor, Klub Motor, Review Tes Motor, Komparasi, Informasi Modifikasi dan Tips Motor Terlengkap Terbaru.</h4>
-		  <a rel="noreferrer" href="https://www.100kpj.com/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.100kpj.com/" class="opl-button" target="_blank">www.100kpj.com</a>
 		</span>
 	  </li>
     </ul>
@@ -194,7 +155,7 @@
 	    <span>
 		  <h2>JagoDangdut</h2>
 		  <h4>Situs yang Mengupas Tuntas Seputar Berita Dangdut, Lirik Lagu Dangdut, Musik Dangdut, Foto Artis Dangdut, Video Artis Dangdut Terkini.</h4>
-		  <a rel="noreferrer" href="https://www.jagodangdut.com/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.jagodangdut.com/" class="opl-button" target="_blank">www.jagodangdut.com</a>
 		</span>
 	  </li>
     </ul>
@@ -207,7 +168,7 @@
 	    <span>
 		  <h2>VLIX</h2>
 		  <h4>Situs yang Menyajikan Kumpulan Video Hot Terbaru Seputar Berita, Olahraga, Otomotif, Gaya Hidup, Digital, Traveling. Serta Streaming tvOne, ANTV, dan Partner.</h4>
-		  <a rel="noreferrer" href="https://www.vlix.id/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.vlix.id/" class="opl-button" target="_blank">www.vlix.id</a>
 		</span>
 	  </li>
     </ul>
@@ -220,7 +181,7 @@
 	    <span>
 		  <h2>VIVA.co.id</h2>
 		  <h4>Situs Berita harian terkini yang menyajikan berita hari ini dan kabar terbaru seputar politik hingga berita lifestyle dan olahraga.</h4>
-		  <a rel="noreferrer" href="https://www.viva.co.id/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.viva.co.id/" class="opl-button" target="_blank">www.viva.co.id</a>
 		</span>
 	  </li>
     </ul>
@@ -233,7 +194,7 @@
 	    <span>
 		  <h2>VIVANews</h2>
 		  <h4>Situs Berita harian terkini yang menyajikan berita hari ini dan kabar terbaru seputar politik hingga berita lifestyle dan olahraga.</h4>
-		  <a rel="noreferrer" href="vivanews/" class="opl-button" target="_blank">Preview</a>
+		  <a rel="noreferrer" href="vivanews/" class="opl-button" target="_blank">www.ovarz.com/vivanews/</a>
 		</span>
 	  </li>
     </ul>
@@ -246,7 +207,7 @@
 	    <span>
 		  <h2>VIVA X Pesbukers ANTV</h2>
 		  <h4>Situs Acara Pencarian Bakat Dari Berbagai Daerah di Indonesia, Untuk Menemukan Talenta Terbaik yang Akan Tampil Bersama Para Pemain Pesbukers ANTV.</h4>
-		  <a rel="noreferrer" href="https://www.viva.co.id/pesbukers" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.viva.co.id/pesbukers" class="opl-button" target="_blank">www.viva.co.id/pesbukers</a>
 		</span>
 	  </li>
     </ul>
@@ -259,20 +220,7 @@
 	    <span>
 		  <h2>VIVA X Daihatsu</h2>
 		  <h4>Situs Daftar Produk dan Daftar Harga Mobil Daihatsu Terbaur, Penawaran Khusus dan Promo 2018, Baca Artikel Seputar Mobil Daihatsu</h4>
-		  <a rel="noreferrer" href="https://www.viva.co.id/daihatsu" class="opl-button" target="_blank">Visit Site</a>
-		</span>
-	  </li>
-    </ul>
-	
-    <ul>
-	  <li class="opl-image content_center">
-	    <amp-img src="<?php echo $base?>/img/portofolio/porto-anisa.jpg" width="160" height="90" layout="responsive" alt="Program Umroh Gratis Bareng Anisa Rahma"></amp-img>
-	  </li>
-	  <li class="opl-info content_center">
-	    <span>
-		  <h2>Program Umroh Gratis Bareng Anisa Rahma</h2>
-		  <h4>Situs Kontes Foto Beauty Of Hijab Berhadiah Umroh Gratis Bareng Anisa Rahma</h4>
-		  <a rel="noreferrer" href="https://m.viva.co.id/umroh-bareng-anisa/" class="opl-button" target="_blank">Visit Site</a>
+		  <a rel="noreferrer" href="https://www.viva.co.id/daihatsu" class="opl-button" target="_blank">www.viva.co.id/daihatsu</a>
 		</span>
 	  </li>
     </ul>
