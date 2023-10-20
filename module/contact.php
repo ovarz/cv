3598c575256eb95f50365b3de14773b116101fa6
ovarz <ovarz_03@yahoo.com>
2023-10-20 08:59:47 +0700

    a

--- module/contact.php
+++ module/contact.php
@@ -53,5 +53,15 @@
       </div>
       <div class="clb-info"><span>facebook.com/aufar.mahardi</span></div>
     </a>
+    
+    <a rel="noreferrer" href="https://github.com/ovarz/" class="contact-list-box contact-list-github" target="_blank">
+      <div class="clb-icon">
+	    <i class="fab fa-github">&#xf09b</i>
+        <div class="clb-arrow">
+          <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path></svg>
+        </div>
+      </div>
+      <div class="clb-info"><span>github.com/ovarz</span></div>
+    </a>
   </div>
 </section>
