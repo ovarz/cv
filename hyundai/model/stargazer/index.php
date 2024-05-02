9f0c2d56d28653286c2b833988f168410f887ee6
ovarz <ovarz_03@yahoo.com>
2024-05-02 15:53:49 +0700

    best deals

--- hyundai/model/stargazer/index.php
+++ hyundai/model/stargazer/index.php
@@ -57,6 +57,47 @@
 	  <div class="section-title">
         <div class="section-title-primary">Best Deals For You</div>
       </div>
+	  <div class="best-deal-container">
+        <?php 
+          $deal_array = array();
+          $deal_array[]=array(
+            'deal_icon'=>'payment',
+            'deal_title'=>'Low Down Payment',
+            'deal_desc'=>'Hyundai Finance in the form of Low Interest 2.4% for 3 Years with a DP of 20%, apart from that you can also enjoy a low interest program for a tenor of 1-7 years'
+          );
+          $deal_array[]=array(
+            'deal_icon'=>'resale',
+            'deal_title'=>'Resale Value Guarantee',
+            'deal_desc'=>'Hyundai guarantees a resale value of 70% of the purchase price if the owner trades in the vehicle for a new Hyundai vehicle within the third year of ownership.'
+          );
+          $deal_array[]=array(
+            'deal_icon'=>'smartdeal',
+            'deal_title'=>'Hyundai Smartdeal',
+            'deal_desc'=>'Hyundai SmartDeal is a smart financing solution to own Hyundai with lower cost up to 50%, worry-less of maintenance cost, and benefit of guaranteed 70% resale value for trade in to new Hyundai car directly after settling the last installment.'
+          );
+          $deal_array[]=array(
+            'deal_icon'=>'protection',
+            'deal_title'=>'Customer Protection',
+            'deal_desc'=>'Medical treatment cost guarantee for drivers and passengers, in the event of an accident requiring medical treatment at the hospital.'
+          );
+          foreach($deal_array as $deal_list){
+        ?>
+		  <div class="bdc-box">
+		    <div class="bdc-icon content_center">
+			  <div class="bdc-icon-circle content_center">
+			    <?php require ($_SERVER['HY'].'img/icon/deal-'.$deal_list['deal_icon'].'.svg')?>
+			  </div>
+			</div>
+			<h2 class="bdc-title"><?php echo($deal_list['deal_title'])?></h2>
+			<h3 class="bdc-desc"><?php echo($deal_list['deal_desc'])?></h3>
+		  </div>
+        <?php } ?>	
+	  </div>
+	  <div class="gateway-bottom content_center">
+        <span class="gateway-list">
+          <a title="Explore Hyundai Deals" class="btn gateway-button" href="">Explore Hyundai Deals</a>
+        </span>
+      </div>
 	</span>
   </section>
   
