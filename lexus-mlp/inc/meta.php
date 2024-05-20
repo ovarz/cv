18d486744854a22722a25150f1c0bcfd0a024e24
ovarz <ovarz_03@yahoo.com>
2024-05-21 00:05:22 +0700

    kerangka lexus-mlp

---
+++ lexus-mlp/inc/meta.php
@@ -0,0 +1,35 @@
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex, follow">		
+<link rel="preconnect" href="https://www.aufarmahardi.com">
+<link rel="dns-prefetch" href="https://www.aufarmahardi.com" />
+<link rel="preconnect" href="https://ajax.googleapis.com">
+<link rel="dns-prefetch" href="https://ajax.googleapis.com"/>
+<link href="img/favicon.ico?<?php echo $version; ?>" rel="icon" type="image/ico" />
+<title><?php echo $site_title; ?></title>
+
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Bold.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Book.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Light.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Regular.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Bold.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Book.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Light.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Regular.woff" rel="preload"/>
+	
+<link rel="preload" href="css/rancak.css?<?php echo $version; ?>" as="style">
+<link rel="preload" as="script" href="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+
+<style><?php require ($_SERVER['HY'].'css/font.css')?></style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>');</script>
+
+<link rel="stylesheet" type="text/css" href="css/rancak.css?<?php echo $version; ?>"/>
+
+<?php if($page == 'spk') { ?>
+  <link rel="preload" href="css/spk.css?<?php echo $version; ?>" as="style">
+  <link rel="stylesheet" type="text/css" href="css/spk.css?<?php echo $version; ?>"/>
+<?php } ?>
+
+</head>
+<body>
