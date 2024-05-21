1b89b6d4f26b66598e601d06cea73b54fd6df280
ovarz <ovarz_03@yahoo.com>
2024-05-21 18:17:34 +0700

    a

--- lexus-mlp/index.php
+++ lexus-mlp/index.php
@@ -9,9 +9,20 @@
     <img class="rancak-form-bg" src="img/<?php echo $page; ?>.jpg"/>
   </div>
   <div class="rancak-form-box">
-    <div class="rancak-form-input" style="top:14.4vw; left:59.2vw;">10 September 2024</div>
-    <div class="rancak-form-input" style="top:17.0vw; left:58.9vw;">xxxx-xxxx-xxxx-xxxx</div>
-    <div class="rancak-form-input" style="top:19.5vw; left:58.9vw;">LEX-00000</div>
+    <div aria-label="Date" class="rancak-form-input" style="top:14.1vw; left:59.2vw;">10 September 2024</div>
+    <div aria-label="SPK No." class="rancak-form-input" style="top:16.6vw; left:58.9vw;">xxxx-xxxx-xxxx-xxxx</div>
+    <div aria-label="Cust. Single ID" class="rancak-form-input" style="top:19.1vw; left:58.9vw;">LEX-00000</div>
+	
+    <div aria-label="Customer Name" class="rancak-form-input" style="width:28vw; height:4vw; top:27.7vw; left:19.2vw;">
+	  Aufar Mahardi
+	</div>
+    <div aria-label="Customer Name" class="rancak-form-input" style="width:28vw; height:2vw; top:31.8vw; left:19.2vw;">
+	  Neo Fusion Indonesia
+	</div>
+	
+    <div aria-label="Correspondent Address" class="rancak-form-input" style="width:28vw; height:6vw; top:39.4vw; left:19.2vw;">
+	  Jl. Tebet Timur Dalam VIII H No.3, RT.11/RW.6, Tebet Tim., Kec. Tebet, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12820
+	</div>
   </div>
 </div>
 <?php require ($_SERVER['HY'].'inc/base-bottom.php')?>
