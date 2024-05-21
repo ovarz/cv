4c7f2f67e346a70a8d3531d77476dfa404b219a8
ovarz <ovarz_03@yahoo.com>
2024-05-21 14:11:30 +0700

    a

--- lexus-mlp/index.php
+++ lexus-mlp/index.php
@@ -5,6 +5,13 @@
 <?php require ($_SERVER['HY'].'inc/meta.php')?>
 <h1 class="hide"><?php echo $site_title; ?></h1>
 <div class="rancak-container">
-  tes
+  <div class="rancak-form">
+    <img class="rancak-form-bg" src="img/<?php echo $page; ?>.jpg"/>
+  </div>
+  <div class="rancak-form-box">
+    <div class="rancak-form-input" style="top:14.4vw; left:59.2vw;">10 September 2024</div>
+    <div class="rancak-form-input" style="top:17.0vw; left:58.9vw;">xxxx-xxxx-xxxx-xxxx</div>
+    <div class="rancak-form-input" style="top:19.5vw; left:58.9vw;">LEX-00000</div>
+  </div>
 </div>
 <?php require ($_SERVER['HY'].'inc/base-bottom.php')?>
