eb5fcd36835bd89e5628726962c9cd74c8eb3590
ovarz <ovarz_03@yahoo.com>
2024-05-23 14:41:02 +0700

    a

--- lexus-mlp/index.php
+++ lexus-mlp/index.php
@@ -24,8 +24,8 @@
 	</div>
 	
     <div aria-label="Request tax invoice" aria-choice="No" class="rancak-form-input rancak-form-yesno" style="top:34.3vw; left:19.2vw;">
-	  <div aria-label="Yes" class="rancak-form-checklist" style="left:59.2vw;"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
-	  <div aria-label="No" class="rancak-form-checklist" style="left:79.2vw;"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	  <div aria-label="Yes" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	  <div aria-label="No" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
 	</div>
 	
     <div aria-label="NPWP" class="rancak-form-input rancak-form-npwp" style="top:36.9vw; left:19.3vw;">
@@ -62,6 +62,79 @@
 	
     <div aria-label="Contact Person phone" class="rancak-form-input" style="width:28vw; height:2vw; top:55.8vw; left:19.2vw;">
 	  62 000000000000
+	</div>
+	
+    <div aria-label="STNK Name" class="rancak-form-input" style="width:28vw; height:4vw; top:28.8vw; left:64.7vw;">
+	  Aufar Mahardi
+	</div>
+	
+    <div aria-label="STNK Address" class="rancak-form-input" style="width:28vw; height:4vw; top:32.9vw; left:64.7vw;">
+	  Jl. Tebet Timur Dalam VIII H No.3, RT.11/RW.6, Tebet Tim., Kec. Tebet, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12820
+	</div>
+	
+    <div aria-label="Plate No Request" aria-choice="Yes" class="rancak-form-input rancak-form-yesno" style="top:39.5vw; left:64.7vw;">
+	  <div aria-label="Yes" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	  <div aria-label="No" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	</div>
+      <div aria-label="Plate No 1" class="rancak-form-input" style="width:28vw; height:2vw; top:43.3vw; left:65.8vw;">
+        B 14 RIN
+      </div>
+      <div aria-label="Plate No 2" class="rancak-form-input" style="width:28vw; height:2vw; top:45.3vw; left:65.8vw;">
+        D 1616 IT
+      </div>
+      <div aria-label="Plate No 3" class="rancak-form-input" style="width:28vw; height:2vw; top:47.4vw; left:65.8vw;">
+        P 461 KK
+      </div>
+	
+    <div aria-label="Note" class="rancak-form-input" style="width:28vw; height:5.5vw; top:51.6vw; left:64.7vw; line-height:1.8vw;">
+	  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed mi eget metus hendrerit ultrices. Fusce blandit, ex eu euismod placerat, quam diam rhoncus tortor, in pellentesque dui lectus ac augue. Sed vel metus urna. Cras quis ultrices erat.
+	</div>
+	
+    <div aria-label="Car Type" class="rancak-form-input rancak-form-cartype" style="width:26vw; height:2vw; top:58.8vw; left:19.2vw;">
+	  <div class="rancak-form-cartype-name">UX 250h F SPORT</div>
+	  <div class="rancak-form-cartype-total">000</div>
+	</div>
+	
+    <div aria-label="Exterior Color" class="rancak-form-input" style="width:28vw; height:2vw; top:60.85vw; left:19.2vw;">
+	  Silver Metallic
+	</div>
+	
+    <div aria-label="Interior Color" class="rancak-form-input" style="width:28vw; height:2vw; top:62.9vw; left:19.2vw;">
+	  Black Carbon
+	</div>
+	
+    <div aria-label="Note" class="rancak-form-input" style="width:28vw; height:9.5vw; top:73.8vw; left:19.2vw; line-height:1.95vw;">
+	  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed mi eget metus hendrerit ultrices. Fusce blandit, ex eu euismod placerat, quam diam rhoncus tortor, in pellentesque dui lectus ac augue. Sed vel metus urna. Cras quis ultrices erat.
+	</div>
+	
+    <div aria-label="Frame No." class="rancak-form-input" style="width:36vw; top:84.7vw; left:11.8vw;">xxxx-xxxx-xxxx-xxxx</div>
+	
+    <div aria-label="Engine No." class="rancak-form-input" style="width:36vw; top:87.4vw; left:11.8vw;">xxxx-xxxx-xxxx-xxxx</div>
+	
+    <div aria-label="OTR Price" class="rancak-form-input" style="width:24.5vw; height:7.5vw; top:64.5vw; left:68vw; line-height:1.8vw;">IDR 000.000.000.000</div>
+	
+    <div aria-label="Special Appreciation" class="rancak-form-input" style="width:24.5vw; height:1.9vw; top:73.0vw; left:68vw; line-height:1.8vw;">IDR 000.000.000</div>
+	
+    <div aria-label="Total Price" class="rancak-form-input" style="width:24.5vw; height:1.9vw; top:75.9vw; left:68vw; line-height:1.8vw;">IDR 000.000.000.000</div>
+	
+    <div aria-label="Choose Payment" aria-choice="Lease" class="rancak-form-input rancak-form-choosepayment" style="top:78.8vw; left:68.9vw;">
+	  <div aria-label="Cash" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	  <div aria-label="Lease" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?><span>Adira Finance</span></div>
+	</div>
+	
+    <div aria-label="Signature" class="rancak-form-signature" style="top:126.1vw; left:7.3vw;">
+	  <div class="rancak-form-signature-box">
+	    <img src="img/sample/signature-1.png"/>
+	  </div>
+	  <div class="rancak-form-signature-box">
+	    <img src="img/sample/signature-1.png"/>
+	  </div>
+	  <div class="rancak-form-signature-box">
+	    <img src="img/sample/signature-1.png"/>
+	  </div>
+	  <div class="rancak-form-signature-box">
+	    <img src="img/sample/signature-1.png"/>
+	  </div>
 	</div>
   </div>
 </div>
