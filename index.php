d955d54530155a4e14a2df1c64bb7a1034d3046f
ovarz <ovarz_03@yahoo.com>
2024-05-23 14:44:32 +0700

    cv

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php $web_type='default'; require ('inc/base.php')?>
-<?php require ('inc/header.php')?>
+<?php $web_type='default'; require ('cv/inc/base.php')?>
+<?php require ('cv/inc/header.php')?>
 <style amp-custom>
-  <?php require ('css/rancak.css')?>
+  <?php require ('cv/css/rancak.css')?>
 </style>
 </head>
 <body>
 <amp-analytics config="https://www.googletagmanager.com/amp.json?id=G-FRRHFE09BY" data-credentials="include"></amp-analytics>
 <div id="top-page"></div>
 <div class="site-container">
-  <?php require ('module/headline.php')?>
-  <?php require ('module/profile.php')?>
-  <?php require ('module/experience.php')?>
-  <?php require ('module/skill.php')?>
+  <?php require ('cv/module/headline.php')?>
+  <?php require ('cv/module/profile.php')?>
+  <?php require ('cv/module/experience.php')?>
+  <?php require ('cv/module/skill.php')?>
 </div>
 <div class="bottom-container">
-  <?php require ('module/contact.php')?>
-  <?php require ('module/portfolio.php')?>
+  <?php require ('cv/module/contact.php')?>
+  <?php require ('cv/module/portfolio.php')?>
 </div>
-<?php require ('inc/footer.php')?>
+<?php require ('cv/inc/footer.php')?>
