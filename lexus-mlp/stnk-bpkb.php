55d994ab1cf60e9d68eb5cb602d2db0610a840f9
ovarz <ovarz_03@yahoo.com>
2024-05-31 18:17:38 +0700

    pluit & stnk-bpkb
    belum selesai

---
+++ lexus-mlp/stnk-bpkb.php
@@ -0,0 +1,271 @@
+<?php 
+  $page='stnk-bpkb';
+  require ('inc/base.php')
+?>
+<?php require ($_SERVER['HY'].'inc/meta.php')?>
+<h1 class="hide"><?php echo $site_title; ?></h1>
+<div class="rancak-container">
+  <div class="rancak-form">
+    <img class="rancak-form-bg" src="img/<?php echo $page; ?>.jpg"/>
+  </div>
+  <div class="rancak-form-box rancak-form-stnk-bpkb">
+    <div aria-label="Nama Customer" class="rancak-form-input" style="top:19.9vw; left:30.2vw;">Aufar Mahardi</div>
+	
+    <div aria-label="Nama pada SPK" class="rancak-form-input" style="top:22.2vw; left:30.2vw;">Razov</div>
+	
+    <div aria-label="Nomor Rangka Kendaraan" class="rancak-form-input" style="top:24.5vw; left:30.2vw;">XXXX XXXX XXXX XXXX</div>
+	
+    <div aria-label="Nomor Mesin Kendaraan" class="rancak-form-input" style="top:26.9vw; left:30.2vw;">XXXX XXXX XXXX XXXX</div>
+	
+    <div aria-label="Nama pada STNK" class="rancak-form-input" style="top:32.3vw; left:30.2vw;">Altair Ibn La-Ahad</div>
+	
+    <div aria-label="Alamat" class="rancak-form-input" style="top:34.6vw; left:30.2vw;">Kavling Melati No.11, RT.9/RW.3, Bangka, Kec. Mampang Prpt., Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12730</div>
+	
+    <div aria-label="Nama PIC & Nomor Telepon" class="rancak-form-input" style="top:39.3vw; left:30.2vw;">Altair (62 800 0000 0000)</div>
+	
+    <div aria-label="Alamat pengiriman STNK & BPKB" class="rancak-form-input" style="top:41.7vw; left:30.2vw;">Kavling Melati No.11, RT.9/RW.3, Bangka, Kec. Mampang Prpt., Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12730</div>
+	
+    <div aria-label="KTP" class="rancak-form-input rancak-form-doclist" style="top:51.0vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Fotocopy NPWP, TDP dan SIUP" class="rancak-form-input rancak-form-doclist" style="top:55.5vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf" style="height:1.5vw;">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date" style="height:1.5vw;">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf" style="height:1.5vw;">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date" style="height:1.5vw;">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Fotocopy Surat Keterangan Domisili Perusahaan" class="rancak-form-input rancak-form-doclist" style="top:57.4vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Surat Kuasa Mobil Baru" class="rancak-form-input rancak-form-doclist" style="top:59.7vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Surat Kuasa Pengambilan BPKB" class="rancak-form-input rancak-form-doclist" style="top:62.0vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Esek-esek Nomor Mesin dan Nomor Rangka" class="rancak-form-input rancak-form-doclist" style="top:69.1vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="KTP Asli" class="rancak-form-input rancak-form-doclist" style="top:71.5vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="STNK Asli" class="rancak-form-input rancak-form-doclist" style="top:73.9vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="BPKB Asli" class="rancak-form-input rancak-form-doclist" style="top:76.2vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Esek-esek Nomor Mesin dan Nomor Rangka" class="rancak-form-input rancak-form-doclist" style="top:80.5vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="STNK Asli" class="rancak-form-input rancak-form-doclist" style="top:82.8vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="BPKB Asli" class="rancak-form-input rancak-form-doclist" style="top:85.2vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Fotocopy NPWP, TDP dan SIUP" class="rancak-form-input rancak-form-doclist" style="top:87.5vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Fotocopy Surat Keterangan Domisili Perusahaan" class="rancak-form-input rancak-form-doclist" style="top:89.9vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-3.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-4.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Surat Kuasa Mobil Baru" class="rancak-form-input rancak-form-doclist" style="top:92.2vw; left:67.8vw;">
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-1.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">08/08/2024</div>
+	  </div>
+	  <div class="rfdl-column">
+        <div class="rfdl-box rfdl-paraf">
+          <img src="img/sample/signature-2.png"/>
+        </div>
+        <div class="rfdl-box rfdl-date">28/08/2024</div>
+	  </div>
+	</div>
+	
+    <div aria-label="Pilihan Nomor Polisi 1" class="rancak-form-input" style="top:100.0vw; left:10.4vw; width:24vw;">B 14 RIN</div>
+	
+    <div aria-label="Pilihan Nomor Polisi 2" class="rancak-form-input" style="top:102.3vw; left:10.4vw; width:24vw;">D 1616 IT</div>
+	
+    <div aria-label="Pilihan Nomor Polisi 3" class="rancak-form-input" style="top:104.6vw; left:10.4vw; width:24vw;">P 4G1 KK</div>
+	
+    <div aria-label="Pilihan Nomor Polisi 4" class="rancak-form-input" style="top:107.0vw; left:10.4vw; width:24vw;">AD 418 U</div>
+	
+    <div aria-label="Pilihan Nomor Polisi 5" class="rancak-form-input" style="top:109.3vw; left:10.4vw; width:24vw;">N 456 OR</div>
+	
+    <div aria-label="NU/U" aria-choice="opt1" class="rancak-form-input rancak-form-yesno rancak-form-updown" style="top:100.0vw; left:61.4vw;">
+	  <div aria-label="opt1" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	  <div aria-label="opt2" class="rancak-form-checklist"><?php require ($_SERVER['HY'].'img/check.svg')?></div>
+	</div>
+	
+    <div aria-label="Nama BJ/PIC" class="rancak-form-input" style="top:109.3vw; left:10.4vw; width:24vw;">N 456 OR</div>
+  </div>
+</div>
+<?php require ($_SERVER['HY'].'inc/base-bottom.php')?>
