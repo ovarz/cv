0c619eeb9c575a8c1f110d3545ee8f0cd2307474
ovarz <ovarz_03@yahoo.com>
2024-06-21 16:45:35 +0700

    slicing form MLP opsi 2

---
+++ lexus-mlp/inc/meta-2.php
@@ -0,0 +1,34 @@
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex, follow">		
+<link rel="preconnect" href="https://www.aufarmahardi.com">
+<link rel="dns-prefetch" href="https://www.aufarmahardi.com" />
+<link rel="preconnect" href="https://ajax.googleapis.com">
+<link rel="dns-prefetch" href="https://ajax.googleapis.com"/>
+<link href="img/favicon.ico?<?php echo $version; ?>" rel="icon" type="image/ico" />
+<title><?php echo $site_title; ?></title>
+
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Bold.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Book.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Light.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Regular.woff2" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Bold.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Book.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Light.woff" rel="preload"/>
+<link as="font" crossorigin="anonymous" href="fonts/Nobel-Regular.woff" rel="preload"/>
+
+<!--
+<link rel="preload" href="img/afi.jpg" as="image">
+<link rel="preload" href="img/ananta.jpg" as="image">
+<link rel="preload" href="img/laras.jpg" as="image">
+-->	
+	
+<link rel="preload" href="img/<?php echo $page; ?>.jpg" as="image">
+
+<style>
+  <?php require ($_SERVER['HY'].'css/font.css')?>
+  <?php require ($_SERVER['HY'].'css/rancak-2.css')?>
+</style>
+
+</head>
+<body>
---
+++ lexus-mlp/menteng.php
@@ -0,0 +1,61 @@
+<?php 
+  $page='menteng';
+  require ('inc/base.php')
+?>
+<?php require ($_SERVER['HY'].'inc/meta-2.php')?>
+<h1 class="hide"><?php echo $site_title; ?></h1>
+<div class="rancak-container">
+
+
+
+  <div class="top-default">
+    <div class="top-default-logo">
+	  <?php require ($_SERVER['HY'].'img/logo.svg')?>
+	</div>
+	<div class="top-default-title"><?php echo $page; ?> gallery</div>
+  </div>
+  
+  
+  
+  <div class="header">
+    <div class="header-left">
+	  <b>PT. ASTRA INTERNATIONAL Tbk</b>
+	  <b class="header-left-dealer">lexus <?php echo $page; ?> gallery</b>
+	  <p>Proklamasi No.35, Central Jakarta - 10320</p>
+	  <p>Tel: (021) 390-1325, Mon-Fri : 8.30 am - 4.30 pm, Sat : 08.00 am - 02.00 pm</p>
+	</div>
+	<div class="header-right">
+	  <div class="header-right-form">
+	    <div class="hrf-label">Date</div>
+	    <div class="hrf-separator">:</div>
+	    <div class="input-result hrf-box hrf-box-outline">30 September 2024</div>
+	  </div>
+	  <div class="header-right-form">
+	    <div class="hrf-label">SPK No.</div>
+	    <div class="hrf-separator">:</div>
+	    <div class="input-result hrf-box hrf-box-solid">XXXX-XXXX-XXXX-XXXX</div>
+	  </div>
+	  <div class="header-right-form">
+	    <div class="hrf-label">Cust.Single ID</div>
+	    <div class="hrf-separator">:</div>
+	    <div class="input-result hrf-box hrf-box-solid">LXID-00000</div>
+	  </div>
+	</div>
+  </div>
+  
+  
+  
+  <div class="mainform">
+    <div class="mainform-head"><b>Lexus Order Form</b></div>
+	<div class="mainform-side mainform-left">
+	  left
+	</div>
+	<div class="mainform-side mainform-right">
+	  right
+	</div>
+  </div>
+  
+  
+  
+</div>
+<?php require ($_SERVER['HY'].'inc/base-bottom.php')?>
