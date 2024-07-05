9e162cfca5449abdf8e845d3314042a5ff502486
ovarz <ovarz_03@yahoo.com>
2024-07-05 16:42:32 +0700

    update syarat ketentuan form menteng

--- lexus-mlp/opt2-menteng-empty.php
+++ lexus-mlp/opt2-menteng-empty.php
@@ -420,7 +420,7 @@
 		  <li>Harga yang tercantum dalam Formulir Pemesanan Lexus ini tidak mengikat.</li>
 		  <li>Formulir Pemesanan Lexus ini bukan Bukti Tanda Terima Pembayaran.</li>
 		  <li>
-		    Formulir Pemesanan Lexus ini dianggap SAH jika:
+		    Formulir Pemesanan Lexus ini dianggap SAH jika :
 			<ul class="mainform-info-list mainform-info-letterup">
 			  <li>Ditandatangani oleh Pelanggan</li>
 			  <li>Disetujui oleh Pejabat Cabang</li>
@@ -428,7 +428,7 @@
 			</ul>
 		  </li>
 		  <li>
-		    Pembayaran dengan menggunakan cek/transfer harus ditujukan kepada PT. Astra International Tbk. dan dianggap sah apabila diterbitkan kuitansi oleh PT. Atra International Tbk dan apabila pembayaran telah diterima di rekening PT. Astra International Tbk pada:
+		    Pembayaran dengan menggunakan cek/transfer harus ditujukan kepada PT. Astra International Tbk. dan dianggap sah apabila diterbitkan kuitansi oleh PT. Astra International Tbk dan apabila pembayaran telah diterima di rekening PT. Astra International Tbk pada:
 			<ul class="mainform-info-list mainform-info-square">
 			  <li>Bank Permata - Cabang Royal Sunter : 0701859820</li>
 			  <li>Bank BCA - Cabang Menara BCA Thamrin : 2050069853</li>
@@ -441,10 +441,11 @@
 		    Berdasarkan peraturan pemerintah tentang perlindungan data pribadi, dengan menandatangani Formulir Pemesanan Lexus ini, Pelanggan menyatakan dan mengikatkan diri bahwa:
 			<ul class="mainform-info-list mainform-info-letterlow">
 			  <li>Pelanggan memahami dan menyetujui bahwa PT. Astra International Tbk. dan PT. Toyota-Astra Motor (selaku Lexus Distributor) akan menerapkan sistem, prosedur, dan alat teknis untuk mengambil tindakan yang wajar dalam melindungi data pribadi yang diberikan oleh Pelanggan pada Formulir Pemesanan Penjualan Lexus.</li>
-			  <li>Pelanggan telah memahami dan menyetujui bahwa data yang diberikan akan dikumpulkan, dikelola, dan digunakan oleh PT. Astra International Tbk., PT. Toyota-Astra Motor (Lexus Distributor), atau; pihak lain yang ditunjuk oleh PT. Astra International Tbk. atau PT. Toyota-Astra Motor (Lexus Distributor);</li>
+			  <li>Pelanggan telah memahami dan menyetujui bahwa data yang diberikan akan dikumpulkan, dikelola, dan digunakan oleh PT. Astra International Tbk., PT. Toyota-Astra Motor (Lexus Distributor), atau pihak lain yang ditunjuk oleh PT. Astra International Tbk. atau PT. Toyota-Astra Motor (Lexus Distributor) di wilayah pemasaran kami;</li>
 			  <li>Pelanggan telah membaca, memahami, dan menyetujui syarat dan ketentuan terkait data pribadi sebagaimana tercantum dalam https://www.lexus.co.id/en/miscellaneous/legal-privacy.html</li>
-			  <li>PT. Astra International Tbk. dan PT. Toyota-Astra Motor (Lexus Distributor) memungkinkan untuk memberikan akses kepada afiliasi perusahaan untuk menggunakan data, berkomunikasi dengan pelanggan melalui berbagai saluran, dan memberikan informasi yang bermanfaat bagi pelanggan, serta melakukan studi untuk meningkatkan pengalaman produk & layanan berdasarkan preferensi pelanggan;</li>
-			  <li>Dalam hal tertentu, Pelanggan mewakili pihak lain dan memberikan data terkait dengan pihak yang diwakilinya, Pelanggan dengan ini menyatakan bahwa pihaknya telah memperoleh persetujuan dari pihak yang diwakilinya untuk memberikan data tersebut dan mengikat pihak yang diwakilinya dengan syarat dan ketentuan sebagaimana tercantum dalam Formulir Pemesanan Lexus.</li>
+			  <li>PT. Toyota-Astra Motor(Lexus Distributor) memungkinkan untuk memberikan akses kepada pihak yang bekerja sama dengan PT. Toyota-Astra Motor(Lexus Distributor) untuk menggunakan data, berkomunikasi dengan pelanggan melalui berbagai saluran, dan memberikan informasi yang bermanfaat bagi pelanggan, serta melakukan studi untuk meningkatkan pengalaman produk & layanan berdasarkan preferensi pelanggan;</li>
+			  <li>Dalam hal tertentu, Pelanggan mewakili pihak lain dan memberikan data terkait dengan pihak yang diwakilinya, Pelanggan dengan ini menyatakan bahwa pihaknya telah memperoleh persetujuan dari pihak yang diwakilinya untuk memberikan data tersebut dan mengikat pihak yang diwakilinya dengan syarat dan ketentuan sebagaimana tercantum dalam Formulir Pemesanan Lexus. Pelanggan bertanggung jawab penuh atas pencantuman, penggunaan, termasuk kebenaran data pribadi tersebut, serta membebaskan PT. Astra International Tbk. dan/atau PT. Toyota-Astra Motor (Lexus Distributor) dari dan atas semua tanggung jawab, kerugian, klaim yang akan atau mungkin timbul akibat pemcantuman dan/atau penggunaan data pribadi di luar data Pelanggan dalam formulir Pemesan ini berdasarkan peraturan perundang-undangan yang berlaku.</li>
+			  <li>Dalam hal Pelanggan ingin memperbaiki data, mengakhiri pemrosesan, menarik persetujuan, mengajukan keberatan, membatasi pemrosesan data dirinya secara proporsional, menghapus, serta hak lain sesuai dengan Peraturan Perundang-Undangan, Pemesan dapat mengajukan permohonan secara tertulis kepada PT. Astra International Tbk. dan/atau PT. Toyota-Astra Motor (Lexus Distributor).</li>
 			</ul>
 		  </li>
 		</ul>
@@ -480,10 +481,11 @@
 		    Based on government regulations regarding personal data protection, by signing this Lexus Order Form the customer declares and binds himself that:
 			<ul class="mainform-info-list mainform-info-letterlow">
 			  <li>The customer understood and agreed that PT. Astra International Tbk. and PT. Toyota-Astra Motor (as Lexus Distributor) will implement system, procedure, and technical tools to take reasonable action to protect the personal data which provided by Customer on Lexus Sales Order Form.</li>
-			  <li>The customer has understood and agreed that the data provided will be collect, manage, and utilize by PT. Astra International Tbk., PT. Toyota-Astra Motor (Lexus Distributor), or other parties appointed by PT. Astra International Tbk.;</li>
+			  <li>The customer has understood and agreed that the data provided will be collect, manage, and utilize by PT. Astra International Tbk. and PT. Toyota-Astra Motor (Lexus Distributor), or other parties appointed by PT. Toyota-Astra Motor (Lexus Distributor) in our marketing territory;</li>
 			  <li>The customer has read, understood and agreed to the terms and conditions related to personal data as stated in https://www.lexus.co.id/en/miscellaneous/legal-privacy.html;</li>
-			  <li>PT. Astra International Tbk. and PT. Toyota-Astra Motor (Lexus Distributor) allowed to grant access to company affiliation to use the data, to communicate with customer through multiple channel, and to provide information which beneficial for the customer, and also conduct study to improve product & services experience based on customer preferences.</li>
-			  <li>At the certain case, that the customer represents other party and provides data related to the party he represents, the customer hereby declares that his party has obtained approval from the party he represents to provide such data and binds the party he represents to the terms and conditions as stated in the Lexus Order Form.</li>
+			  <li>PT. Astra International Tbk. and PT. Toyota-Astra Motor (Lexus Distributor) allowed to grant access to party who cooperates with PT. Toyota-Astra Motor (Lexus Distributor) to use the data, to communicate with customer through multiple channel, and to provide information which beneficial for the customer, and also conduct study to improve product & services experience based on customer preferences.</li>
+			  <li>At the certain case, that the customer represents other party and provides data related to the party he represents, the customer hereby declares that his party has obtained approval from the party he represents to provide such data and binds the party he represents to the terms and conditions as stated in the Lexus Order Form. Customers are fully responsible for the inclusion, usage, and accuracy of such personal data, as well as discharging PT. Astra International Tbk. and/or PT. Toyota-AstraMotor (Lexus Distributor) from and for all responsibilities, losses and claims that will or may occur as a result of the inclusion and/or usage of personal data other than Customer Data in this Order Form based on applicable laws and regulations.</li>
+			  <li>In the event that the Customer wishes to correct the data, end processing, withdraw consent, submit and objection, limit the processing of his or her data proportionally, delete, as well as other rights in accordance with the Laws and Regulations, the Customer can submit a written request to PT. Astra International Tbk. and/or and PT. Toyota-Astra Motor (Lexus Distributor).</li>
 			</ul>
 		  </li>
 		</ul>
