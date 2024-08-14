ff7ba91b765fe60bd48a0d88f72fa9172cba30c6
ovarz <ovarz_03@yahoo.com>
2024-08-14 10:04:29 +0700

    Update information.php

--- fiersa/information.php
+++ fiersa/information.php
@@ -47,7 +47,7 @@
         ?>
           <div class="main-slide">
 		    <a title="<?php echo($slidebox_list['slidebox_title'])?>" class="main-slide-thumb rancak-lazy thumb-loading"
-			href="<?php echo($slidebox_list['slidebox_link'])?>.php">
+			href="<?php echo($slidebox_list['slidebox_link'])?>" target="_blank">
 			  <img class="lazyload" src="img/sample/slide-<?php echo($slidebox_list['slidebox_img'])?>.jpg" />
 			</a>
           </div>
