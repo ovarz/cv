1a6a528f336ca93ef8360cd7c3d0b46e936a04d2
ovarz <ovarz_03@yahoo.com>
2024-12-16 20:13:50 +0700

    viva16

--- hut-viva-16/asset/kurnen.js
+++ hut-viva-16/asset/kurnen.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
   var video = $('#viva16')[0];
   setTimeout(function() {
     video.muted = false;
-    video.play().catch(function(error) {
-      console.log("Autoplay failed, waiting for user interaction.");
-    });
   },1000);
   
   setTimeout(function(){
     //$('button').html("<video autoplay><source src=\"asset/closing.mp4\" type=\"video/mp4\"></video>");
 	
 	$('button').click(function(){
-	  $(this).html("<video autoplay><source src=\"asset/closing.mp4\" type=\"video/mp4\"></video>");
+	  $(this).html("<video autoplay><source src=\"asset/closing.mp4?v2\" type=\"video/mp4\"></video>");
       return false;
     });
-  },5000);
+  },70000);
 });
--- hut-viva-16/opening.php
+++ hut-viva-16/opening.php
@@ -9,7 +9,6 @@
   <link rel="preload" href="asset/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
   <link rel="preload" href="asset/kurnen.css?<?php echo $version; ?>" as="style">
   <link rel="preload" href="asset/jquery.js" as="script">
-  <link rel="preload" href="asset/kurnen.js?<?php echo $version; ?>" as="script">
   
   <link rel="stylesheet" type="text/css" href="asset/kurnen.css?<?php echo $version; ?>"/>
 </head>
@@ -17,13 +16,26 @@
 
 
 <button>
-<video id="viva16" autoplay muted><source src="asset/opening.mp4" type="video/mp4"></video>
+<video id="viva16" autoplay muted><source src="asset/opening.mp4?<?php echo $version; ?>" type="video/mp4"></video>
 </button>
 
 
 <script src="asset/jquery.js"></script>
-<script src="asset/kurnen.js?<?php echo $version; ?>"></script>
-
+<script>
+$(document).ready(function() {
+  var video = $('#viva16')[0];
+  setTimeout(function() {
+    video.muted = false;
+  },2000);
+  
+  setTimeout(function(){
+    $('button').click(function(){
+	  $(this).html("<video autoplay><source src=\"asset/closing.mp4?<?php echo $version; ?>\" type=\"video/mp4\"></video>");
+      return false;
+    });
+  },70000);
+});
+</script>
 
 
 </body>
