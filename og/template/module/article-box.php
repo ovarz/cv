9d33487cc1c2135df6641bf290578f5dc1ca14f5
ovarz <ovarz_03@yahoo.com>
2025-05-14 23:04:43 +0700

    a

--- og/template/module/article-box.php
+++ og/template/module/article-box.php
@@ -12,8 +12,8 @@
       <h2 class="article-title-detail"><?php echo $random_title[array_rand($random_title)];?></h2>
     </a>
 	<div class="article-misc">
-      <a title="<?php echo $random_kanal[array_rand($random_kanal)];?>" class="article-kanal" href="subdomain.php">
-        <h3 class="article-kanal-detail"><?php echo $random_kanal[array_rand($random_kanal)];?></h2>
+      <a title="<?php echo $random_cate[array_rand($random_cate)];?>" class="article-cate" href="subdomain.php">
+        <h3 class="article-cate-detail"><?php echo $random_cate[array_rand($random_cate)];?></h2>
       </a>
       <h4 class="article-other article-<?php echo $article_misc;?>">
         <?php if($article_misc == 'time'){ ?>4 jam lalu<?php } ?>
