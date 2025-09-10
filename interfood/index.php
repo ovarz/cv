5b36e3dedb75d1357495775ad8f2cf1a15479950
ovarz <ovarz_03@yahoo.com>
2025-09-10 12:43:39 +0700

    9

--- interfood/index.php
+++ interfood/index.php
@@ -28,7 +28,13 @@
   <section class="content-center home-product">
     <div class="section-separator section-separator-top"><?php require ($_SERVER['IF'].'template/img/icon/separator.svg')?></div>
     <div class="site-container">
-	  <?php require ($_SERVER['IF'].'template/module/h2h3.php')?>
+	  <?php 
+	    $title_id='Produk';
+		$subtitle_id='Kami menyediakan beragam produk makanan dan minuman, mulai dari camilan manis seperti cokelat dan permen, hingga berbagai bahan untuk kebutuhan bisnis FnB maupun memasak di rumah.';
+	    $title_en='Product'; 
+		$subtitle_en='We offer a wide variety of food & beverage products. From sweets like chocolate & confectionaries, ingredients for both FnB businesses and home cooks, etc.';
+	    require ($_SERVER['IF'].'template/module/h2h3.php')
+	  ?>
 	  
       <div class="home-product-list">
         <ul class="hpl-container">
@@ -95,9 +101,125 @@
   
   
   
-  <section class="content-center">
+  <section class="content-center home-distribution">
     <div class="site-container">
-	  <?php require ($_SERVER['IF'].'template/module/h2h3.php')?>
+	  <?php 
+	    $title_id='Distribusi';
+		$subtitle_id='Indonesia ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    $title_en='Distribution'; 
+		$subtitle_en='English ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    require ($_SERVER['IF'].'template/module/h2h3.php')
+	  ?>
+	</div>
+  </section>
+  
+  
+  
+  <section class="content-center home-importer">
+	<div class="section-separator section-separator-top"><?php require ($_SERVER['IF'].'template/img/icon/separator.svg')?></div>
+    <div class="site-container">
+	  <?php 
+	    $title_id='Pengimpor';
+		$subtitle_id='Kami mengimpor produk dari';
+	    $title_en='Importer'; 
+		$subtitle_en='We import products from';
+	    require ($_SERVER['IF'].'template/module/h2h3.php')
+	  ?>
+	  
+      <div class="home-importer-list">
+        <ul class="hil-container">
+          <?php 
+            $home_importer_array = array();
+            $home_importer_array[]=array(
+              'hil_name_id'=>'Singapore',
+              'hil_name_en'=>'Singapura',
+              'hil_img'=>'sg',
+            );
+            $home_importer_array[]=array(
+              'hil_name_id'=>'Malaysia',
+              'hil_name_en'=>'Malaysia',
+              'hil_img'=>'my',
+            );
+            $home_importer_array[]=array(
+              'hil_name_id'=>'Thailand',
+              'hil_name_en'=>'Thailand',
+              'hil_img'=>'th',
+            );
+            $home_importer_array[]=array(
+              'hil_name_id'=>'China',
+              'hil_name_en'=>'Cina',
+              'hil_img'=>'cn',
+            );
+            $home_importer_array[]=array(
+              'hil_name_id'=>'Turki',
+              'hil_name_en'=>'Turkey',
+              'hil_img'=>'ty',
+            );
+            $home_importer_array[]=array(
+              'hil_name_id'=>'Lainnya',
+              'hil_name_en'=>'Many more',
+              'hil_img'=>'wd',
+            );
+            foreach($home_importer_array as $home_importer_list){
+          ?>
+            <li class="hil-column">
+              <div class="hil-box img-frame thumb-loading">
+                <img title="Foto <?php echo($home_importer_list['hil_name_en'])?>" class="lazyload" 
+				data-original="template/img/photo-<?php echo($home_importer_list['hil_img'])?>.jpg">
+                <div class="hil-overlay content-center">
+				  <div class="hil-overlay-info">
+				    <div class="hil-overlay-icon content-center">
+					  <?php require ($_SERVER['IF'].'template/img/flag-'.$home_importer_list['hil_img'].'.svg')?>
+					</div>
+                    <h2 class="hil-overlay-name">
+                      <span class="text-id"><?php echo($home_importer_list['hil_name_id'])?></span>
+                      <span class="text-en"><?php echo($home_importer_list['hil_name_en'])?></span>
+                    </h2>
+				  </div>
+				</div>
+              </div>
+            </li>
+          <?php } ?>
+        </ul>
+      </div>
+	</div>
+	<div class="section-separator section-separator-bottom"><?php require ($_SERVER['IF'].'template/img/icon/separator.svg')?></div>
+  </section>
+  
+  
+  
+  <section class="content-center home-brand">
+    <div class="site-container">
+	  <?php 
+	    $title_id='Brand Ternama yang Kami Wakili';
+		$subtitle_id='Indonesia ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    $title_en='Top Brands We Represent'; 
+		$subtitle_en='English ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    require ($_SERVER['IF'].'template/module/h2h3.php')
+	  ?>
+	</div>
+  </section>
+  
+  
+  
+  <section class="content-center latest-news">
+    <div class="site-container">
+	  <?php 
+	    $title_id='Berita Terkini';
+		$subtitle_id='Indonesia ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    $title_en='Latest News'; 
+		$subtitle_en='English ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.';
+	    require ($_SERVER['IF'].'template/module/h2h3.php')
+	  ?>
+	  
+	  <div class="news-list">
+	    <?php for ($i=1; $i<=4; $i++){ ?>
+	      <?php require ($_SERVER['IF'].'template/module/news-box.php')?>
+		<?php } ?>
+	  </div>
+	  
+	  <?php $more_type='button'; $more_label_id='Berita Lainnya'; $more_label_en='More News'; $more_link=''; 
+	  require ($_SERVER['IF'].'template/module/more.php')?>
 	</div>
   </section>
   
--- interfood/template/module/h2h3.php
+++ interfood/template/module/h2h3.php
@@ -1,10 +1,10 @@
 <div class="section-label">
   <h2 class="section-title">
-    <span class="text-id">ID Ipsum Dolor</span>
-    <span class="text-en">EN Ipsum Dolor</span>
+    <span class="text-id"><?php echo $title_id; ?></span>
+    <span class="text-en"><?php echo $title_en; ?></span>
   </h2>
   <h3 class="section-subtitle">
-    <span class="text-id">ID ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.</span>
-    <span class="text-en">EN ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis lacinia mauris at volutpat. Integer a porttitor orci, sed scelerisque risus.</span>
+    <span class="text-id"><?php echo $subtitle_id; ?></span>
+    <span class="text-en"><?php echo $subtitle_en; ?></span>
   </h3>
 </div>
